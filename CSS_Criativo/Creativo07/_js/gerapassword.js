e3683f8c973410dfb3c112441ef0a89f0647cf0c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-20 10:54:58 +0100

    HTML, JS e Css - Creativo07 - Strong Random Password

--- CSS_Criativo/Creativo07/_js/gerapassword.js
+++ CSS_Criativo/Creativo07/_js/gerapassword.js
@@ -1,3 +1,13 @@
+let InpPassword = document.querySelector('input#password')
+
 function getPassword(){
-    let chars = ""
+    let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&/()=?@{}[]'
+    let tamPassword = 16
+    let password = ""
+
+    for (let i=0; i<tamPassword; i++){
+        var numAletoreo = Math.floor(Math.random() * chars.length)
+        password += chars.substring(numAletoreo,numAletoreo+1)
+    }
+    InpPassword.value = password
 }
